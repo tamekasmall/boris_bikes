0e836953a1e4cc52ab18a80169437b8d14a7c87c
Tameka <tamekasmall@hotmail.com>
2019-01-09 15:20:09 +0000

    10 Making Docking Station Release Bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,5 @@
 class Bikes
   def working?
+    true
   end
 end
--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,7 @@
+require_relative 'bike' #use require for files in other folder, use require_relative for files in same folder and domt use "load" unless you want to load all in that file
+
 class DockingStation
   def release_bike
-
+    Bikes.new
   end
 end
